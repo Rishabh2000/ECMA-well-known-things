69949c52ee187dbf6d3cebfa94facddc1bdea3e2
SatanLabs , Inc <46850618+satanlabs@users.noreply.github.com>
2019-08-12 16:47:58 +0530

    added objects-array add , delete , check methods
    - Add an element to an array uniquely.
    - Remove an element from an array.
    - Check if an element exists in an array.

--- Utils.js
+++ Utils.js
@@ -1,1 +1,37 @@
 
+//############################# ARRAY METHODS START #############################
+
+const addElementToArrayUniquely = (element, array) => {
+  //will not add element, if it already exists
+  let elementAlreadyExist = false;
+  array.map(data => {
+    if (data.id == element.id) {
+      elementAlreadyExist = true;
+    }
+  });
+  if (!elementAlreadyExist) {
+    array.push(element);
+  }
+  return array;
+};
+
+const deleteElementFromArray = (element, elementArray) => {
+  let newElementArray = [];
+
+  elementArray.filter(data => {
+    if (data.id != element.id) {
+      newElementArray.push(data);
+    }
+  });
+  return newElementArray;
+};
+
+const checkIfElementExistsInArray = (element, elementArray) => {
+  let exists = false;
+  elementArray.map(data => {
+    if (data.id == element.id) exists = true;
+  });
+  return exists;
+};
+
+//############################# ARRAY METHODS STOP #############################
