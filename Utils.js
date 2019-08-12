32c32c6e7af89244391b8f8e6cc4053b9b19de68
SatanLabs , Inc <46850618+satanlabs@users.noreply.github.com>
2019-08-12 16:59:04 +0530

    Added code for DOWNLOAD FILE in the same page.

--- Utils.js
+++ Utils.js
@@ -48,3 +48,37 @@ const checkIfStringContainsPattern = (string, pattern) => {
     return string.includes(pattern);
 }
 // Eg: checkIfStringContainsPattern("apple" , "app" ) , will return TRUE.
+
+
+
+//############################# DOWNLOAD FILE START #############################
+
+// DOWNLOAD FILE in the same page 
+//this can be used to download a file, without the need to open new-tab.
+const downloadURI = (fileURL, fileName = "someFile.txt") => {
+    var link = document.createElement("a");
+    link.download = fileName;
+    link.href = fileURL;
+    link.click();
+}
+
+
+function downloadPDF(pdfBase64Data, fileName) {
+      /*This method will download a PDF.*/
+    const linkSource = `data:application/pdf;base64,${pdfBase64Data}`;
+    downloadURI(linkSource, fileName);
+}
+
+function downloadPng(pngImageBase64Data, fileName) {
+    /*This method will download a png-image.*/
+    const linkSource = `data:image/png;base64, ${pngImageBase64Data}`;
+    downloadURI(linkSource, fileName);
+}
+
+function downloadJpg(jpgImageBase64Data, fileName) {
+    /*This method will download a jpeg-image.*/
+    const linkSource = `data:image/jpeg;base64, ${jpgImageBase64Data}`;
+    downloadURI(linkSource, fileName);
+}
+
+//############################# DOWNLOAD FILE STOP #############################
